b00d2f5b194216ff06f8a296bbea9337e821fb7b
Colaborador <colaborador@N094693.local>
2022-09-08 11:17:18 -0300

    feat/changeClassroomModule

--- labenu-system/src/index.ts
+++ labenu-system/src/index.ts
@@ -1,6 +1,8 @@
 import app from "./app";
 import addClassroom from "./endpoints/addClassroom";
+import changeClassroomModule from "./endpoints/changeClassroomModule";
 import getClassrooms from "./endpoints/getClassrooms";
 
 app.post("/classroom/new", addClassroom);
 app.get("/classrooms", getClassrooms);
+app.put("/classroom/:id", changeClassroomModule);
