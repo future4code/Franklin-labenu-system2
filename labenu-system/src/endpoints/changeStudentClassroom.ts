dd6fe22cb58664345c9b917478a44e65f26aa178
Victor Ramon Firmo Moreira <victor.moreira@grupoboticario.com.br>
2022-09-13 16:41:23 -0300

    Adicionando as ultimas funcionalidades e requests

---
+++ labenu-system/src/endpoints/changeStudentClassroom.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+import { Classroom } from "../classes/Classroom";
+import { Student } from "../classes/Student";
+import connection from "../conection";
+
+export default async function changeStudentClassroom(
+  req: Request,
+  res: Response
+): Promise<void> {
+  try {
+    const { classroom_id } = req.body as Student;
+
+    if (!classroom_id) {
+      res.statusCode = 422;
+      throw new Error("Insira o id da turma para prosseguir");
+    }
+
+    await connection("lbn_system_students")
+      .update({
+        classroom_id,
+      })
+      .where({
+        id: req.params.id,
+      });
+
+    res.status(201).send(`Parabéns! O aluno agora está cadastrado na turma de id ${classroom_id}`);
+  } catch (error: any) {
+    if (res.statusCode === 200) {
+      console.log(error);
+      res.status(500).send({ message: "Internal server error" });
+    } else {
+      console.log(error);
+      res.send({ message: error.message });
+    }
+  }
+}
--- labenu-system/src/index.ts
+++ labenu-system/src/index.ts
@@ -3,20 +3,20 @@ import addClassroom from "./endpoints/addClassroom";
 import addStudent from "./endpoints/addStudent";
 import addTeacher from "./endpoints/addTeacher";
 import changeClassroomModule from "./endpoints/changeClassroomModule";
+import changeStudentClassroom from "./endpoints/changeStudentClassroom";
 import changeTeacher from "./endpoints/changeTeachers";
 import getTeachers from "./endpoints/getAllTeachers";
 import getClassrooms from "./endpoints/getClassrooms";
 import searchStudentByName from "./endpoints/searchStutendByName";
 
-
 app.post("/classroom/new", addClassroom);
 app.get("/classrooms", getClassrooms);
 app.put("/classroom/:id", changeClassroomModule);
 
+app.get("/student/:name", searchStudentByName);
 app.post("/student/new", addStudent);
-app.put("/student/name", searchStudentByName);
+app.put("/student/:id", changeStudentClassroom);
 
 app.get("/teacher", getTeachers);
 app.post("/teacher/new", addTeacher);
 app.put("/teacher/:id", changeTeacher);
-
